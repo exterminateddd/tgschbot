0a5fb21918a62d106c5f89b343bb47c692f1b1fe
exter <exterminatedisback@gmail.com>
2024-09-25 19:23:36 +0300

    fix env.py

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 WEEKDAYS = 'Z понедельник вторник среда четверг пятница суббота воскресенье'.split(' ')
 week_type = lambda isocal: "верхняя" if isocal.week%2==0 else "нижняя"
-is_user_admin = lambda user: user.name.lstrip('@') in open('admins.txt', 'r', encoding='utf-8').readlines()
+is_user_admin = lambda user: user.name.lstrip('@') in map(lambda name: name.strip().strip('\n').strip().lstrip('@'), open('admins.txt', 'r', encoding='utf-8').readlines())
